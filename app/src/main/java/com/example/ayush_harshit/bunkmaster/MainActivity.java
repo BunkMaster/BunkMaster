9e64500c8c038ef626034fc325263a9413bf111e
ayush <adas162534@gmail.com>
2018-02-26 16:23:25 +0530

    Added items in a subject and add_subjects xml

--- app/src/main/java/com/example/ayush_harshit/bunkmaster/MainActivity.java
+++ app/src/main/java/com/example/ayush_harshit/bunkmaster/MainActivity.java
@@ -3,11 +3,9 @@ package com.example.ayush_harshit.bunkmaster;
 /**
  * Created by ayush on 5/2/18.
  */
-import com.example.ayush_harshit.bunkmaster.Adapters.RecyclerViewAdapter;
-import com.example.ayush_harshit.bunkmaster.R;
+import com.example.ayush_harshit.bunkmaster.Adapters.Subject;
+import com.example.ayush_harshit.bunkmaster.Adapters.SubjectListAdapter;
 
-import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -18,12 +16,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 
 import java.util.ArrayList;
+
+import butterknife.BindViews;
+import butterknife.ButterKnife;
 
 /**
  * The launchpad activity for this sample project. This activity launches other activities that
@@ -35,19 +34,34 @@ public class MainActivity extends AppCompatActivity {
      * demonstrates this sample).
      */
     FloatingActionButton floatingActionButton;
-    RecyclerView recyclerView;
+    RecyclerView mRecyclerView;
     RecyclerView.LayoutManager recyclerViewLayoutManager;
-    RecyclerViewAdapter recyclerViewHorizontalAdapter;
+    /*SubjectListAdapter recyclerViewHorizontalAdapter;
     LinearLayoutManager horizontalLayoutManager;
-    ArrayList<String> options;
     View childView;
-    int recyclerViewItemPosition;
+    int recyclerViewItemPosition;*/
+
+    //@BindViews(R.id.recyclerView) RecyclerView mRecyclerView;
+    private SubjectListAdapter mAdapter;
+
+    public ArrayList<Subject> subjects;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView1);
+
+        //ButterKnife.bind(this);
+        //Intent intent = getIntent();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        recyclerViewLayoutManager = new LinearLayoutManager(getBaseContext());
+        mRecyclerView.setLayoutManager(recyclerViewLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new SubjectListAdapter(getBaseContext(), subjects);
+        mRecyclerView.setAdapter(mAdapter);
+
         floatingActionButton = (FloatingActionButton)findViewById(R.id.addSubjects);
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -58,88 +72,55 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        recyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(recyclerViewLayoutManager);
+        //recyclerViewLayoutManager = new LinearLayoutManager(getApplicationContext());
+        //mRecyclerView.setLayoutManager(recyclerViewLayoutManager);
 
         //Add items to the arraylist
-        AddItemsToRecyclerViewArrayList();
-
-        recyclerViewHorizontalAdapter = new RecyclerViewAdapter(options);
-
-        //horizontalLayoutManager = new LinearLayoutManager(MainActivity.this, LinearLayoutManager.HORIZONTAL, false);
-        //recyclerView.setLayoutManager(horizontalLayoutManager);
-
-        recyclerView.setAdapter(recyclerViewHorizontalAdapter);
-
-        recyclerView.addOnItemTouchListener(new RecyclerView.OnItemTouchListener() {
-
-            GestureDetector gestureDetector = new GestureDetector(MainActivity.this,
-                    new GestureDetector.SimpleOnGestureListener() {
-                        @Override
-                        public boolean onSingleTapUp(MotionEvent e) {
-                            return super.onSingleTapUp(e);
-                        }
-                    });
-
-            @Override
-            public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-                childView = rv.findChildViewUnder(e.getX(), e.getY());
-
-                if (childView != null && gestureDetector.onTouchEvent(e)) {
-                    recyclerViewItemPosition = rv.getChildAdapterPosition(childView);
-                    Toast.makeText(MainActivity.this, options.get(recyclerViewItemPosition),
-                            Toast.LENGTH_SHORT).show();
-                }
-                return false;
-            }
-
-            @Override
-            public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-                childView = rv.findChildViewUnder(e.getX(), e.getY());
-                recyclerViewItemPosition = rv.getChildAdapterPosition(childView);
-                /*if (recyclerViewItemPosition == 0) {
-                    startActivity(new Intent(MainActivity.this,
-                            CrossfadeActivity.class));
-                }
-                if (recyclerViewItemPosition == 1) {
-                    startActivity(new Intent(MainActivity.this,
-                            CardFlipActivity.class));
-                }
-                if (recyclerViewItemPosition == 2) {
-                    startActivity(new Intent(MainActivity.this,
-                            ScreenSlideActivity.class));
-                }
-                if (recyclerViewItemPosition == 3) {
-                    startActivity(new Intent(MainActivity.this,
-                            ZoomActivity.class));
-                }
-                if (recyclerViewItemPosition == 4) {
-                    startActivity(new Intent(MainActivity.this,
-                            LayoutChangesActivity.class));
-                }
-                */
-            }
-
-            @Override
-            public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-
-            }
-        });
+        AddSubjectsToRecyclerViewArrayList();
 
     }
 
     // function to add items in RecyclerView.
-    public void AddItemsToRecyclerViewArrayList() {
+    public void AddSubjectsToRecyclerViewArrayList() {
 
-        options = new ArrayList<>();
-        options.add("Simple Crossfade");
-        options.add("Card Flip");
-        options.add("Screen Slide");
-        options.add("Zoom");
-        options.add("Layout change");
-        options.add("Mood change");
-        options.add("Dress change");
-        options.add("Topic change");
+        subjects = new ArrayList<Subject>();
+        subjects.add(new Subject("CS-309","Simple Crossfade",
+                "95%"));
+        //mAdapter.notifyDataSetChanged();
+        //mAdapter.notifyItemRangeChanged(0, subjects.size());
+
+        subjects.add(new Subject("CS-309","Simple Crossfade",
+                "95%"));
+        //mAdapter.notifyDataSetChanged();
+        mAdapter.notifyItemRangeChanged(0, subjects.size());
+
+        /*subjects.add(new Subject("CS-309","Simple Crossfade",
+                "95%"));
+        //mAdapter.notifyDataSetChanged();
+
+        subjects.add(new Subject("CS-309","Simple Crossfade",
+                "95%"));
+        //mAdapter.notifyDataSetChanged();
+
+        subjects.add(new Subject("CS-309","Simple Crossfade",
+                "95%"));
+        //mAdapter.notifyDataSetChanged();
+
+        subjects.add(new Subject("CS-309","Simple Crossfade",
+                "95%"));
+        //mAdapter.notifyDataSetChanged();
+
+        subjects.add(new Subject("CS-309","Simple Crossfade",
+                "95%"));
+        //mAdapter.notifyDataSetChanged();
+
+        subjects.add(new Subject("CS-309","Simple Crossfade",
+                "95%"));
+        mAdapter.notifyDataSetChanged();*/
 
     }
 }
+
+/*
+
+ */
