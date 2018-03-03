1acc3ef11561cd64f71ee6528a36f7d48636e815
ayush <adas162534@gmail.com>
2018-03-03 12:19:37 +0530

    Added back button in the actionbar (AddASubject)

--- app/src/main/java/com/example/ayush_harshit/bunkmaster/AddASubject.java
+++ app/src/main/java/com/example/ayush_harshit/bunkmaster/AddASubject.java
@@ -1,10 +1,13 @@
 package com.example.ayush_harshit.bunkmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 public class AddASubject extends AppCompatActivity {
@@ -15,7 +18,25 @@ public class AddASubject extends AppCompatActivity {
         setContentView(R.layout.activity_add_subjects);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
 
 }
